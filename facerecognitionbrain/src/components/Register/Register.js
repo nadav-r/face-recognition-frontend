8e839c7ccc60237aeb23fad8779ad300b7bd3c5f
nadav-r <36492247+nadav-r@users.noreply.github.com>
2019-04-22 13:33:37 +0300

    fix signin and change host

--- facerecognitionbrain/src/components/Register/Register.js
+++ facerecognitionbrain/src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component /*({onRouteChange}) => */{
 /*		if(email ==='' || password==='' || name===''){
 			return;
 		}*/
-		fetch('http://localhost:3000/register', {
+		fetch('https://calm-island-32009.herokuapp.com/register', {
 			method:'post',
 			headers:{'Content-Type':'application/json'},
 			body: JSON.stringify({
